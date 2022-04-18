26ca40db27bd2340a7514eaf49a65d7c70db0a45
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-04-17 22:39:52 -0700

    Remove jquery

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
+const liveLinkBtn = document.querySelector('.js-live-link-button');
+const repoLinkBtn = document.querySelector('.js-repo-link-button');
 
-function handleLiveLinkClicked() {
-  $('.js-live-link-button').click(function () {
-    window.open('https://tcatt71.github.io/quiz-app-final/', '_blank');
-  });
-}
-
-function handleRepoLinkClicked() {
-  $('.js-repo-link-button').click(function () {
-    window.open('https://github.com/tcatt71/quiz-app-final', '_blank');
-  });
-}
-
-function handlePortfolio() {
-  handleLiveLinkClicked();
-  handleRepoLinkClicked();
-}
-
-$(handlePortfolio);
+liveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/quiz-app-final/', '_blank'));
+repoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/quiz-app-final', '_blank'));
