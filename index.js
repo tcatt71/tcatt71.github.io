28ca62a12fbaf4ec9cf3c7c2f4594fe3ea10d4ea
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-06-09 02:06:01 -0700

    Add webapp dashboard project to portfolio

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const quizAppLiveLinkBtn = document.querySelector('.js-quiz-app-live-link-btn');
 const quizAppRepoLinkBtn = document.querySelector('.js-quiz-app-repo-link-btn');
 const employeeDirectoryLiveLinkBtn = document.querySelector('.js-employee-directory-live-link-btn');
 const employeeDirectoryRepoLinkBtn = document.querySelector('.js-employee-directory-repo-link-btn');
+const webAppDashboardLiveLinkBtn = document.querySelector('.js-web-app-live-link-btn');
+const webAppDashboardRepoLinkBtn = document.querySelector('.js-web-app-repo-link-btn');
 
 quizAppLiveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/quiz-app-final/', '_blank'));
 quizAppRepoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/quiz-app-final', '_blank'));
 employeeDirectoryLiveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/empoyee-directory/', '_blank'));
+employeeDirectoryRepoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/empoyee-directory', '_blank'));
 webAppDashboardLiveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/webapp-dashboard/', '_blank'));
+webAppDashboardRepoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/webapp-dashboard', '_blank'));
