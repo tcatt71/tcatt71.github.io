5ebc118967d0253ac1146f20a50dccada4afa003
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-06-09 01:10:07 -0700

    Wire Employee Directory buttons to appropriate links

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const quizAppLiveLinkBtn = document.querySelector('.js-quiz-app-live-link-btn');
 const quizAppRepoLinkBtn = document.querySelector('.js-quiz-app-repo-link-btn');
+const employeeDirectoryLiveLinkBtn = document.querySelector('.js-employee-directory-live-link-btn');
+const employeeDirectoryRepoLinkBtn = document.querySelector('.js-employee-directory-repo-link-btn');
 
 quizAppLiveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/quiz-app-final/', '_blank'));
 quizAppRepoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/quiz-app-final', '_blank'));
 employeeDirectoryLiveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/empoyee-directory/', '_blank'));
+employeeDirectoryRepoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/empoyee-directory', '_blank'));
