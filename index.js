1b04c3650df3121b3e521e6f047dfad0ba88d649
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-06-09 01:01:05 -0700

    Give seahawk quiz buttons project specific variable names

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const liveLinkBtn = document.querySelector('.js-live-link-button');
-const repoLinkBtn = document.querySelector('.js-repo-link-button');
+const quizAppLiveLinkBtn = document.querySelector('.js-quiz-app-live-link-btn');
+const quizAppRepoLinkBtn = document.querySelector('.js-quiz-app-repo-link-btn');
 
-liveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/quiz-app-final/', '_blank'));
-repoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/quiz-app-final', '_blank'));
+quizAppRepoLinkBtn.addEventListener('click', () => window.open('https://github.com/tcatt71/quiz-app-final', '_blank'));
+employeeDirectoryLiveLinkBtn.addEventListener('click', () => window.open('https://tcatt71.github.io/empoyee-directory/', '_blank'));
